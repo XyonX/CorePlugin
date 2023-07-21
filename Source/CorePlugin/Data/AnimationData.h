237dfe0b6930276bf934f0fd47ec31724ea18d78
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-07-21 23:39:36 +0530

    Added Animation data struct

---
+++ Source/CorePlugin/Data/AnimationData.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+//#include "AnimationData.Generated.h
+
+USTRUCT(BlueprintType)
+struct FAnimationData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MovementSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bIsJumping;
+
+	// Add more animation-related variables as needed
+};
